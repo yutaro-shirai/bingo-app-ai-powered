054479fdb43bf3033cb15ac9c0ac004baf575f48
Yutaro SHIRAI <shirai_yu@tdc.co.jp>
2025-11-26 17:46:21 +0900

    feat: initialize backend with NestJS, Prisma, and Dockerized PostgreSQL for database management.

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [GameModule],
+  imports: [DatabaseModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
